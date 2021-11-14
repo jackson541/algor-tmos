9b599527ffbc1952216044001f00ff58410ebe73
Jackson Alves <jacksonalves541@gmail.com>
2021-11-14 12:33:21 -0300

    change buscador_sequencial to return index of value

--- busca_sequencial.py
+++ busca_sequencial.py
@@ -6,17 +6,17 @@ def buscador_sequencial(lista, valor):
     """
     for index in range(len(lista)):
         if lista[index] == valor:
-            return True
-    return False
+            return index
+    return -1
 
 
 
 if __name__ == '__main__':
-    lista = [numero for numero in range(19999999)]
+    lista = [numero for numero in range(1, 19999999)]
     agora = time.time()
     
-    assert buscador_sequencial(lista, 19999990) == True
-    assert buscador_sequencial(lista, 999999999) == False
+    assert buscador_sequencial(lista, 19999990) == 19999989
+    assert buscador_sequencial(lista, 999999999) == -1
     
     print(f"demorou {time.time() - agora} segundos")
 
